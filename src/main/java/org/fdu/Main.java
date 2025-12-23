d9a9374a8166d917e088383e48f63512e4281d13
maddy-daniel <madelynd@student.fdu.edu>
2025-12-22 21:04:30 -0500

    Enter Command method added

--- src/main/java/org/fdu/Main.java
+++ src/main/java/org/fdu/Main.java
@@ -4,14 +4,18 @@ package org.fdu;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        UI ui= new UI();
+
+            //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ui.writeString("Hi!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            ui.writeString("Enter Command:");
+            ui.getLine();
         }
+
     }
 }
--- src/main/java/org/fdu/UI.java
+++ src/main/java/org/fdu/UI.java
@@ -1,10 +1,17 @@
 package org.fdu;
 
+import java.util.Scanner;
+
 public class UI {
 
     public UI(){}
 
     public void writeString(String msg){
         System.out.println(msg);
+    }
+
+    public String getLine(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
