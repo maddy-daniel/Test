bed05e1275c3c1796aa55820adf12eab8e7fd2ac
maddy-daniel <madelynd@student.fdu.edu>
2026-01-20 17:21:49 -0500

    Add JavaFX via Maven

--- src/main/java/org/fdu/Main.java
+++ src/main/java/org/fdu/Main.java
@@ -1,17 +1,19 @@
 package org.fdu;
 
+import javafx.scene.text.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
+//import javafx.scene.text.Font;
+//import javafx.scene.text.FontWeight;
+//import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
